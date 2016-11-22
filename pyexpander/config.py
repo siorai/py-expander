ddb528c778dc422b97012175c6bf66eb258da8f3
Paul <siorai@gmail.com>
2016-11-21 23:36:36 -0500

    Added catagories and relevent info for Google Drive implimentation

--- pyexpander/config.py
+++ pyexpander/config.py
@@ -1,11 +1,12 @@
 import babelfish
 
-# Directories settings.
-TV_PATH = '/tv/directory'
-MOVIE_PATH = '/movies/directory'
-APP_PATH = '/apps/directory'
-MUSIC_PATH = '/music/directory'
-DEFAULT_PATH = '/'
+# Local directory settings.
+TV_PATH = '~/Downloads/Transmission/Organized/TV'
+MOVIE_PATH = '~/Downloads/Transmission/Organized/Movies'
+APP_PATH = '~/Downloads/Transmission/Organized/Programs'
+MUSIC_PATH = '~/Downloads/Transmission/Organized/Music'
+OTHER_PATH = '~/Downloads/Transmission/Organized/Other'
+DEFAULT_PATH = '~/Downloads/Transmission/Organized/'
 
 # Log settings.
 LOGFILE = '/var/log/pyexp.log'
@@ -32,7 +33,16 @@ LANGUAGE_EXTENSIONS = ['.he', '.en']
 # Lists.
 EXTENSIONS_WHITE_LIST = ['.srt', '.mkv', '.avi', '.mp4', '.mov', '.m4v', '.wmv']
 NAMES_BLACK_LIST = ['sample']
-# Directories settings.
+
+# Remote settings.
+
+# Amazon directory settings.
 AMAZON_TV_PATH = '/amazon/tv/path'
 AMAZON_MOVIE_PATH = '/amazon/movies/path'
 ORIGINAL_NAMES_LOG = '/var/log/original_names.log'
+
+# Google Drive settings.
+KEYFILE = '/home/siorai/Python/GoogleDriveFtw!-fc20e0f60d1b.json'
+CLIENT_EMAIL = 'seedbox@virtual-plexus-92702.iam.gserviceaccount.com'
+DELEGATED_EMAIL = 'paul@ladancesafe.org'
+
